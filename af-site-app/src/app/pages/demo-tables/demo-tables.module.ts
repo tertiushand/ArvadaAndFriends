a21de87c55d60ce7ca5552ff824c215fb6671ad6
Tertiushand <braddesmondtaylor@gmail.com>
2018-05-05 19:17:56 -0600

    putting table in a box.

--- af-site-app/src/app/pages/demo-tables/demo-tables.module.ts
+++ af-site-app/src/app/pages/demo-tables/demo-tables.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DemoTablesComponent } from './demo-tables.component';
+import { OneBoxModule } from '../../shared/layout/templates/one-box/one-box.module';
 import { TableModule } from '../../shared/features/table/table.module'
 
 @NgModule({
   imports: [
     CommonModule,
-    TableModule
+    TableModule,
+    OneBoxModule
   ],
   declarations: [DemoTablesComponent]
 })
