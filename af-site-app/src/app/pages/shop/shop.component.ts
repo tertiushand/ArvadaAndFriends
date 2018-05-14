7a4bd2f142d0ad9c146e0038056777f2fa8e82e4
Tertiushand <braddesmondtaylor@gmail.com>
2018-05-13 19:24:56 -0600

    adding some data.

--- af-site-app/src/app/pages/shop/shop.component.ts
+++ af-site-app/src/app/pages/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductInfo } from './product-info.data';
+
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private productInfo: ProductInfo
+  ) { }
 
   ngOnInit() {
   }
